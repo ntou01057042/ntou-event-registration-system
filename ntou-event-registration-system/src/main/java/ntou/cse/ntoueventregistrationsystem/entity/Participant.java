8ac4de644e42d4be3e586e43f294b0d76af3437d
xiaoyo123 <36474975azl@gmail.com>
2023-11-18 16:17:14 +0800

    feat:handle GET request from exporting data to CSV

---
+++ ntou-event-registration-system/src/main/java/ntou/cse/ntoueventregistrationsystem/entity/Participant.java
@@ -0,0 +1,44 @@
+package ntou.cse.ntoueventregistrationsystem.entity;
+
+public class Participant {
+    private String name;
+    private String studentID;
+    private String email;
+    private String phoneNumber;
+
+    public Participant() {
+    }
+
+    public Participant(String name, String studentID, String email, String phoneNumber) {
+        this.name = name;
+        this.studentID = studentID;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getStudentID() {
+        return studentID;
+    }
+    public void setStudentID(String studentID) {
+        this.studentID = studentID;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    
+}
--- ntou-event-registration-system/src/main/java/ntou/cse/ntoueventregistrationsystem/service/EventService.java
+++ ntou-event-registration-system/src/main/java/ntou/cse/ntoueventregistrationsystem/service/EventService.java
@@ -1,11 +1,18 @@
 package ntou.cse.ntoueventregistrationsystem.service;
 
 import ntou.cse.ntoueventregistrationsystem.entity.Event;
+import ntou.cse.ntoueventregistrationsystem.entity.Participant;
 import ntou.cse.ntoueventregistrationsystem.repository.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
 import java.util.List;
+import java.util.*;
+import com.opencsv.CSVWriter;
+
 
 @Service
 public class EventService {
@@ -26,5 +33,19 @@ public class EventService {
 
     public void createEvent(Event event) {
         repository.insert(event);
+    }
+    public void generateCSV(HttpServletResponse response, String id) throws IOException{
+        response.setContentType("text/csv");
+        response.setHeader("Content-Disposition", "attachment; file=report.csv");
+        CSVWriter writer = new CSVWriter(response.getWriter());
+        writer.writeNext(new String[] {"Name", "StudentID", "Email", "PhoneNumber"});
+
+        Event event = repository.findById(id).get();
+        ArrayList<Participant> participant = event.getParticipant();
+
+        for(Participant p : participant)
+            writer.writeNext(new String[] {p.getName(), "\t" + p.getStudentID(), "\t" + p.getEmail(), "\t" + p.getPhoneNumber()});
+        
+        writer.close();
     }
 }
