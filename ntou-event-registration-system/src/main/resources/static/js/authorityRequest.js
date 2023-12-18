736f003060e09bf39b45313f7aadddefd2e2ebf5
vvvvss <songweiyi0318@gmail.com>
2023-12-19 00:40:01 +0800

    feat:delete request after alert user request is rejected

--- ntou-event-registration-system/src/main/resources/static/js/authorityRequest.js
+++ ntou-event-registration-system/src/main/resources/static/js/authorityRequest.js
@@ -16,14 +16,29 @@ function getApplicationRecord(){
             if(response.status != null){
                 if(response.status === "rejected"){
                     window.alert("您的申請已被拒絕");
-
+                    $.ajax({
+                        url:"/requests/" + response.id,
+                        type: "DELETE",
+                        headers: { "Authorization": 'Bearer ' + sessionStorage.getItem("accessToken") },
+                        success: function (){
+                            console.log("刪除成功");
+                            location.reload();
+                        },
+                        error: function(jqXHR, textStatus, errorThrow) {
+                            if (jqXHR.responseText === 'Expired JWT!') {
+                                alert('驗證已過期，請重新登入！');
+                                localStorage.setItem('redirect', 'createEvent.html');
+                                window.location.assign("/html/login.html");
+                            }
+                        }
+                    });
                 }
                 else{
                     for (let i = 0; i < myForm.elements.length; i++) {
                         myForm.elements[i].disabled = true;
                     }
+                    document.getElementById("submitBtn").value = "審核中";
                 }
-                document.getElementById("submitBtn").value = "審核中";
             }
         },
         error: function(jqXHR, textStatus, errorThrow) {
