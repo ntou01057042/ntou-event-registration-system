e012729d20c7d142c88187a5e881a246a2a26b87
vvvvss <songweiyi0318@gmail.com>
2023-12-18 21:35:03 +0800

    feat: admin authority management

--- ntou-event-registration-system/src/main/resources/static/js/authorityRequest.js
+++ ntou-event-registration-system/src/main/resources/static/js/authorityRequest.js
@@ -14,8 +14,14 @@ function getApplicationRecord(){
         success: function (response) {
             console.log("成功");
             if(response.status != null){
-                for (let i = 0; i < myForm.elements.length; i++) {
-                    myForm.elements[i].disabled = true;
+                if(response.status === "rejected"){
+                    window.alert("您的申請已被拒絕");
+
+                }
+                else{
+                    for (let i = 0; i < myForm.elements.length; i++) {
+                        myForm.elements[i].disabled = true;
+                    }
                 }
                 document.getElementById("submitBtn").value = "審核中";
             }
@@ -36,9 +42,12 @@ function submitApplication(){
 
         const formData = new FormData(event.target);
         const obj = Object.fromEntries(formData.entries());
+
+        //
         obj.userId = "65619d69b4fd555ba23b957f";
         obj.name = sessionStorage.getItem("name");
         obj.email = sessionStorage.getItem("email");
+        //
 
         $.ajax({
             url: "/requests",
