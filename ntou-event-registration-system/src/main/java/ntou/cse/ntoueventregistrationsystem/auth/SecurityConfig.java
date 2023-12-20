3641ab81df96129e90ae574bf6c24ac4f8ffb2a3
xiaoyo123 <36474975azl@gmail.com>
2023-12-20 21:50:53 +0800

    fix: security config

--- ntou-event-registration-system/src/main/java/ntou/cse/ntoueventregistrationsystem/auth/SecurityConfig.java
+++ ntou-event-registration-system/src/main/java/ntou/cse/ntoueventregistrationsystem/auth/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/comments").hasAnyAuthority("ADMIN", "ADVANCED", "GENERAL")
                         .requestMatchers(HttpMethod.POST, "/comments/?*").hasAnyAuthority("ADMIN", "ADVANCED", "GENERAL")
                         .requestMatchers(HttpMethod.DELETE, "/comments/?*").hasAnyAuthority("ADMIN")
-                        .requestMatchers(HttpMethod.DELETE, "/comments/resetReport/?*").hasAnyAuthority("ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/comments/resetReport/?*").hasAnyAuthority("ADMIN")
                         .requestMatchers(HttpMethod.POST, "/blocks").hasAnyAuthority("ADMIN", "ADVANCED")
                         .requestMatchers(HttpMethod.GET, "/blocks").hasAnyAuthority("ADMIN", "ADVANCED")
                         .requestMatchers(HttpMethod.DELETE, "/blocks").hasAnyAuthority("ADMIN", "ADVANCED")
